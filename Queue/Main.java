2c449cba3bf5f9687e3eb57c746c27c8f46e86c8
Tolga Akın İnan <115407911+tolgaakininan@users.noreply.github.com>
2022-12-04 12:33:11 +0300

    Add files via upload

--- Queue/Main.java
+++ Queue/Main.java
@@ -1,1 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue kuyruk=new Queue(15);
+        kuyruk.add(33125);
+        kuyruk.add(3512);
+        kuyruk.add(358998);
+        kuyruk.add(3567);
+        kuyruk.add(3554);
+        kuyruk.add(2335);
+        kuyruk.add(305);
+        kuyruk.add(395);
+        kuyruk.add(235);
+        kuyruk.add(135);
+        kuyruk.add(353);
+        kuyruk.add(325);
+        kuyruk.add(353);
+        kuyruk.add(356);
+        kuyruk.add(355);
+        kuyruk.add(95);
+        kuyruk.add(735);
+        kuyruk.add(25);
+        kuyruk.add(15);
+        kuyruk.add(35);
+        kuyruk.add(55);
+        kuyruk.print();
+        System.out.println("remove smt");
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.remove();
+        kuyruk.print();
 
+
+    }
+}
---
+++ Queue/Queue.java
@@ -0,0 +1,49 @@
+public class Queue {
+    private int[] arr;
+    private int size;
+    public Queue(int capacity)
+    {
+        arr=new int[capacity];
+    }
+    public void add(int data)
+    {
+        if(size==arr.length)
+        {
+            int[] tempArr=new int[arr.length*2];
+            System.arraycopy(arr,0,tempArr,0,arr.length);
+            arr=tempArr;
+        }
+        arr[size++]=data;
+    }
+    public int peek()
+    {
+        return arr[0];
+    }
+    public void remove()
+    {
+        if(isEmpty())
+        {
+            System.out.println("List is empty");
+        }
+        for(int i=0;i<size-1;i++)
+        {
+            arr[i]=arr[i+1];
+        }
+        size--;
+    }
+    public void print()
+    {
+        if(isEmpty())
+        {
+            System.out.println("List is empty");
+        }
+        for(int i=0;i<size;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    public boolean isEmpty()
+    {
+        return size==0;
+    }
+}
